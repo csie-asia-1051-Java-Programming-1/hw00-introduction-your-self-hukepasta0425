6cbd7e883c2f99df893414f451169b10be154b28
hukepasta0425 <105021031@live.asia.edu.tw>
2016-09-12 15:49:50 +0800

    Homework
    9/12

---
+++ huke new/src/test01/hukeproject01.java
@@ -0,0 +1,21 @@
+package test01;
+import java.util.*;
+public class hukeproject01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner src=new Scanner(System.in);
+		System.out.println("第一數 :");
+		int a=src.nextInt();
+		System.out.println("第二數 :");
+		int b=src.nextInt();
+		
+		int sum=a+b;
+		System.out.println(sum);
+		
+	}
+
+}
